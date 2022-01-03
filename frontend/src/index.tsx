4b08d0726a7016fbe1ac5a2bb1a85b121d61e147
ekank <ekankr2@naver.com>
2022-01-03 12:10:16 +0900

    install and set universal cookie

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -9,6 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/theme.css'
 import './assets/css/index.css'
 
+
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
