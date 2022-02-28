beeedbd4ff15005269f2e63dae76aeb949e2b77c
ekank <ekankr2@naver.com>
2022-02-28 22:06:07 +0900

    명시적인 코드로 변경

--- frontend/src/api/queryHooks/mypageHooks/buyerMypageHooks.ts
+++ frontend/src/api/queryHooks/mypageHooks/buyerMypageHooks.ts
@@ -10,24 +10,38 @@ export enum BuyerEnum {
 
 export const useGetRecentSituation = () =>
     useQuery(
-        BuyerEnum.RecentSituation,
-        () => axios.get<RecentSituation>('/api/recentsituation')
-            .then((res) => res.data),
+        BuyerEnum.RecentSituation, async () => {
+            const res = await axios.get<RecentSituation>('/api/recentsituation')
+            return res.data
+        }
     )
 
 export const useGetBuyerDashboard = () =>
     useQuery(
-        BuyerEnum.BuyerDashboard,
-        () => axios.get<BuyerDashboard>('/api/dashboard')
-            .then((res) => res.data)
+        BuyerEnum.BuyerDashboard, async () => {
+            const res = await axios.get<BuyerDashboard>('/api/dashboard')
+            return res.data
+        }
     )
+
+// export const useGetSituationDetail = (detailId: number) =>
+//     useQuery(
+//         [BuyerEnum.RecentSituationDetail,detailId],
+//         () => axios.post<Situation>('/api/situationdetail', {
+//             detailId: detailId
+//         }).then((res) => res.data),{
+//             enabled: !!detailId
+//         }
+//     )
 
 export const useGetSituationDetail = (detailId: number) =>
     useQuery(
-        [BuyerEnum.RecentSituationDetail,detailId],
-        () => axios.post<Situation>('/api/situationdetail', {
-            detailId: detailId
-        }).then((res) => res.data),{
+        [BuyerEnum.RecentSituationDetail, detailId], async () => {
+            const res = await axios.post<Situation>('/api/situationdetail', {
+                    detailId: detailId
+                }
+            )
+            return res.data
+        }, {
             enabled: !!detailId
-        }
-    )
+        })
