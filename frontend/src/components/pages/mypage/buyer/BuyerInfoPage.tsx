00c4a4064f09dd4903dff2c18a6b6b9a7ac98309
ekank <ekankr2@naver.com>
2022-03-02 22:56:08 +0900

    nickname added

--- frontend/src/components/pages/mypage/buyer/BuyerInfoPage.tsx
+++ frontend/src/components/pages/mypage/buyer/BuyerInfoPage.tsx
@@ -15,12 +15,12 @@ const BuyerInfoPage: FC = () => {
                     <div className="card__body">
                         <div className="mb-4">
                             <h4 className="mb-3"><i className='bx bx-user mr-2'></i>회원 분류</h4>
-                            <span className="mr-2">
+                            <span className="mr-2 ml-2">
                             <Badge type={"info"} content={"일반"}/>
-                        </span>
+                            </span>
                             <span className="mr-2">
                             <Badge type={"danger"} content={"판다"}/>
-                        </span>
+                            </span>
                             <span className="mr-2">
                             <Badge type={"primary"} content={"판매자"}/>
                         </span>
@@ -28,16 +28,24 @@ const BuyerInfoPage: FC = () => {
                         <Divider/>
                         <div className="mb-4 mt-4">
                             <h4 className="mb-3"><i className='bx bxl-gmail mr-2'></i>이메일</h4>
-                            <div className="mr-2">
+                            <div className="mr-2 ml-3">
                                 <Badge type={"info"} content={"Jimong@gmail.com"}/>
-                                <span className="float-end"><Button text="수정하기"
-                                                                    className="is-info is-small is-outlined"/></span>
+                            </div>
+                        </div>
+                        <Divider/>
+                        <div className="mb-4 mt-4">
+                            <h4 className="mb-3"><i className='bx bx-leaf mr-2'></i>닉네임</h4>
+                            <div className="mr-2 ml-3">
+                                <Badge type="primary" content="페이커"/>
+                                <span className="float-end">
+                                    <Button text="수정하기" className="is-info is-small is-outlined"/>
+                                </span>
                             </div>
                         </div>
                         <Divider/>
                         <div className="mb-4 mt-4">
                             <h4 className="mb-3"><i className='bx bx-calendar mr-2'></i>가입일</h4>
-                            <div className="mr-2">
+                            <div className="mr-2 ml-2">
                                 <Badge type="warning" content="2020년 12월 11일"/>
                             </div>
                         </div>
