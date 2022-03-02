73843fd4b3fb20e9d6a47de3a4882a226d712aed
ekank <ekankr2@naver.com>
2022-03-02 22:51:23 +0900

    email, regdate

--- frontend/src/components/pages/mypage/buyer/BuyerInfoPage.tsx
+++ frontend/src/components/pages/mypage/buyer/BuyerInfoPage.tsx
@@ -13,17 +13,35 @@ const BuyerInfoPage: FC = () => {
                     </div>
                     <Divider/>
                     <div className="card__body">
-
-                        <h4><i className='bx bx-user'></i>회원 분류 :</h4>
-                        <span className="mr-2">
+                        <div className="mb-4">
+                            <h4 className="mb-3"><i className='bx bx-user mr-2'></i>회원 분류</h4>
+                            <span className="mr-2">
                             <Badge type={"info"} content={"일반"}/>
                         </span>
-                        <span className="mr-2">
+                            <span className="mr-2">
                             <Badge type={"danger"} content={"판다"}/>
                         </span>
-                        <span className="mr-2">
+                            <span className="mr-2">
                             <Badge type={"primary"} content={"판매자"}/>
                         </span>
+                        </div>
+                        <Divider/>
+                        <div className="mb-4 mt-4">
+                            <h4 className="mb-3"><i className='bx bxl-gmail mr-2'></i>이메일</h4>
+                            <div className="mr-2">
+                                <Badge type={"info"} content={"Jimong@gmail.com"}/>
+                                <span className="float-end"><Button text="수정하기"
+                                                                    className="is-info is-small is-outlined"/></span>
+                            </div>
+                        </div>
+                        <Divider/>
+                        <div className="mb-4 mt-4">
+                            <h4 className="mb-3"><i className='bx bx-calendar mr-2'></i>가입일</h4>
+                            <div className="mr-2">
+                                <Badge type="warning" content="2020년 12월 11일"/>
+                            </div>
+                        </div>
+                        <Divider/>
                         <div className="card__footer mt-4">
                             <Button text={"홈으로"} className="is-primary is-large is-rounded is-outlined mr-5"/>
                             <Button text={"로그아웃"} className="is-danger is-large is-rounded"/>
