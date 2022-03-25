5e2cd2f7c56d77bd35d8c153fc735bd1de2d1e55
ekankr2 <ekankr2@naver.com>
2022-03-25 18:02:47 +0900

    panda hooks

---
+++ frontend/src/api/queryHooks/mypageHooks/pandaMypageHooks.ts
@@ -0,0 +1,43 @@
+import {useQuery} from "react-query";
+import axios from '../../axiosDefaults'
+import {PandaDashboard, PandaSettlementList, PandaSettlementRequestData, PandaVideoList} from "../types";
+
+export enum PandaMypageKeysEnum {
+    PandaDashboard = 'pandaDashboard',
+    PandaSettlementList = 'pandaSettlementList',
+    PandaVideoList = 'pandaVideoList',
+}
+
+export const useGetPandaDashboard = (year: number) =>
+    useQuery(
+        PandaMypageKeysEnum.PandaDashboard, async () => {
+            const {data} = await axios.post('/api/pandadashboardmain', {
+                year: year
+            })
+            return data as PandaDashboard
+        }
+    )
+
+export const useGetPandaSettlementList = (requestData: PandaSettlementRequestData) =>
+    useQuery(
+        PandaMypageKeysEnum.PandaDashboard, async () => {
+            const {data} = await axios.post('/api/pandadashboard', {
+                startYear: new Date(requestData.startDate).getFullYear(),
+                startMonth: new Date(requestData.startDate).getMonth(),
+                startDay: new Date(requestData.startDate).getDate(),
+                endYear: new Date(requestData.endDate).getFullYear(),
+                endMonth: new Date(requestData.endDate).getMonth(),
+                endDay: new Date(requestData.endDate).getDate(),
+                status: requestData.searchStatus,
+            })
+            return data as PandaSettlementList
+        }
+    )
+
+export const useGetPandaVideoList = () =>
+    useQuery(
+        PandaMypageKeysEnum.PandaVideoList, async () => {
+            const {data} = await axios.get('/api/pandadashboardmovie')
+            return data as PandaVideoList
+        }
+    )
