83b45378f8e28adaf1181a759be4b0e632f07301
ekank <ekankr2@naver.com>
2022-03-28 21:18:48 +0900

    navbar change

--- frontend/src/components/sections/navbar/Navbar.tsx
+++ frontend/src/components/sections/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
-import React, {FC} from 'react';
+import React, {FC, useCallback} from 'react';
 import './navbar.css'
-import {Link, useHistory} from "react-router-dom";
+import {Link, useHistory, useLocation} from "react-router-dom";
 import Dropdown from "../../UI/dropdown/Dropdown";
 import {notification_dummy, user_menu, panda_menu, seller_menu} from "./navbarTypes";
 import Button from "../../UI/Button";
@@ -8,19 +8,20 @@ import {useAuthStore} from "../../../store/authHooks";
 
 const renderNotificationItem = (item: StringObj, index: number) => (
     <div className="notification-item" key={index}>
-        <i className={item.icon}></i>
+        <i className={item.icon}/>
         <span>{item.content}</span>
     </div>
 )
 
 const Navbar: FC = () => {
     const history = useHistory();
+    const location = useLocation()
     const user = useAuthStore(state => state.user)
     const signOut = useAuthStore(state => state.signOut)
 
-    console.log('유저데이타: ',user)
+    console.log('유저데이타: ', location)
 
-    const renderUserToggle = () => (
+    const renderUserToggle = useCallback(() => (
         <div className="topnav__right-user">
             <div className="topnav__right-user__image">
                 {/*<img src={user.image} alt=""/>*/}
@@ -29,48 +30,54 @@ const Navbar: FC = () => {
                 {'안녕하세요'}
             </div>
         </div>
-    )
+    ), [])
 
-    const renderUserMenu = (item: StringObj, index: number) => (
-            <div key={index}>
-                {
-                    item.link === "/logout" ?
-                        <a onClick={() => {
-                            signOut()
-                        }} key={index}>
-                            <div className="notification-item">
-                                <i className={item.icon}></i>
-                                <span>{item.content}</span>
-                            </div>
-                        </a> :
-                        <Link to={item.link} key={index}>
-                            <div className="notification-item">
-                                <i className={item.icon}></i>
-                                <span>{item.content}</span>
-                            </div>
-                        </Link>
-                }
-            </div>
-    )
+    const renderUserMenu = useCallback((item: StringObj, index: number) => (
+        <div key={index}>
+            {
+                item.link === "/logout" ?
+                    <a onClick={() => {
+                        signOut()
+                    }} key={index}>
+                        <div className="notification-item">
+                            <i className={item.icon}/>
+                            <span>{item.content}</span>
+                        </div>
+                    </a> :
+                    <Link to={item.link} key={index}>
+                        <div className="notification-item">
+                            <i className={item.icon}/>
+                            <span>{item.content}</span>
+                        </div>
+                    </Link>
+            }
+        </div>
+    ), [signOut, user])
 
-    const renderAuthMenu = (panda: any, seller: any) => {
-        if(panda && seller) {
+    const renderAuthMenu = useCallback((panda: any, seller: any) => {
+        if (panda && seller) {
             return user_menu
         }
-        if(panda){
+        if (panda) {
             return panda_menu
         }
-        if(seller){
+        if (seller) {
             return seller_menu
         }
         return user_menu
-    }
+    }, [user])
 
     return (
         <nav className="navbar is-spaced has-shadow">
-            <Link className="navbar-item" to={'/'}>
-                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
-            </Link>
+            {location.pathname === '/' ?
+                <a className="navbar-item" href='/'>
+                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
+                </a>
+                : <Link className="navbar-item" to={'/'}>
+                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
+                </Link>
+            }
+
 
             {user ? <div className="navbar-end">
                     <div className="navbar-menu">
@@ -84,7 +91,7 @@ const Navbar: FC = () => {
                         <span className="mr-3">
                             <Dropdown
                                 icon='bx bx-bell'
-                                badge='12'
+                                badge='0'
                                 contentData={notification_dummy}
                                 renderItems={(item: StringObj, index: number) => renderNotificationItem(item, index)}
                                 renderFooter={() => <Link to='/'>View All</Link>}
@@ -96,7 +103,7 @@ const Navbar: FC = () => {
                 <div className="navbar-end">
                     <div className="navbar-menu">
                         <div className="buttons">
-                            <Button text="회원가입" onClick={() => history.push('/signup')} className="is-primary" />
+                            <Button text="회원가입" onClick={() => history.push('/signup')} className="is-primary"/>
                             <Button text="로그인" onClick={() => history.push('/signin')}/>
                         </div>
                     </div>
